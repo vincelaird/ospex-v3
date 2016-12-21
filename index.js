40e63e232faedbdfcdca148bbcf82faa7e15aaad
vincelaird <vincelaird@gmail.com>
2016-12-21 05:30:41 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const express = require('express'),
+      app     = express(),
+      morgan  = require('morgan');
+      
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.listen(process.env.PORT, process.env.IP, () => {
+    console.log('ospex v3 listening');
+});
