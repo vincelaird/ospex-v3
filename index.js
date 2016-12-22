4b2b87d1d7bf5ed440724670004a1456325e36e4
vincelaird <vincelaird@gmail.com>
2016-12-22 20:56:35 +0000

    attempt to add travis take 3 port explicitly defined as 8080

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
-app.listen(process.env.PORT, process.env.IP, () => {
+app.listen(process.env.PORT || 8080, process.env.IP, () => {
     console.log('ospex v3 listening');
 });
 
